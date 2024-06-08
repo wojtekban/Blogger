77e856f55da84d2bf4ffea6801fe9ba5804bc3a2
wojtekban <wojtekban@op.pl>
2024-06-08 16:12:15 +0200

    Moduł 8.3 Testy jednostkowe w xUnit

---
+++ Infrastructure/Identity/UserService.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Identity;
+
+public class UserService
+{
+    public bool IsUserEmailConfirmed(ApplicationUser applicationUser)
+        => applicationUser.EmailConfirmed ? true : false;
+}
---
+++ UserServiceTest.cs
@@ -0,0 +1,43 @@
+﻿namespace UnitTests.Services
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void Is_User_Email_Confirmed_When_Email_Confirmed_Is_True_Returns_True()
+        {
+            // Arrange
+            ApplicationUser user = new()
+            {
+                UserName = "wojtek",
+                EmailConfirmed = true
+            };
+
+            UserService service = new();
+            // Act
+
+            bool isEmailConfirmed = service.IsUserEmailConfirmed(user);
+
+            // Assert
+            Assert.True(isEmailConfirmed);
+        }
+
+        [Fact]
+        public void Is_User_Email_Confirmed_When_Email_Confirmed_Is_False_Returns_False()
+        {
+            // Arrange
+            ApplicationUser user = new()
+            {
+                UserName = "wojtek",
+                EmailConfirmed = false
+            };
+
+            UserService service = new();
+            // Act
+
+            bool isEmailConfirmed = service.IsUserEmailConfirmed(user);
+
+            // Assert
+            Assert.False(isEmailConfirmed);
+        }
+    }
+}
